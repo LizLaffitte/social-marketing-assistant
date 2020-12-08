f7e0c5791f275894e246df0ebaa9caebecb34d2c
lizlaffitte <laffitte.digital@gmail.com>
2020-12-07 20:59:32 -0500

    Implement test for username.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {User.new(username:'Robert')}
+
+  before {subject.save}
+
+  it 'should have a username' do
+    subject.username = nil
+    expect(subject).to_not be_valid
+  end
 end
