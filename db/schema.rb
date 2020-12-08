e58b881ce91af8e45e7b9355f5f6a01b06c0fa7d
lizlaffitte <laffitte.digital@gmail.com>
2020-12-07 20:58:13 -0500

    Add additional attributes/columns to Users table.

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,9 @@ ActiveRecord::Schema.define(version: 2020_12_06_221138) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "password_digest"
+    t.string "email"
+    t.string "role"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
