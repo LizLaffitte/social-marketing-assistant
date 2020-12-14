2a2a70c83aeb744f090e365cf3032ee97aa376c5
lizlaffitte <laffitte.digital@gmail.com>
2020-12-13 21:22:22 -0500

    Incorporate additional user tests.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,15 @@ RSpec.describe User, type: :model do
     subject.username = nil
     expect(subject).to_not be_valid
   end
+
+  it 'should have a unique username' do
+    user = User.create(username:'Sam')
+    subject.username = 'Sam'
+    expect(subject).to_not be_valid
+  end
+
+  it 'should have an email address' do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
 end
