82b03c18f6cc706b836588ea2d8e8476b00bca27
lizlaffitte <laffitte.digital@gmail.com>
2020-12-15 12:55:41 -0500

    Add login controller method.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,5 +2,15 @@ class SessionsController < ApplicationController
     def index 
 
     end
-
+    def create
+        user = User.find_by(username: params[:session][:username])
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            rendser json: user
+        elsif !user
+            render json: {errors: "User doesn't exist"}
+        else 
+            render json: {errors: "Invalid credentials"}
+        end
+    end
 end
