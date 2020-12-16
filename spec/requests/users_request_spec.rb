812b17bc7fdd5d38659791af25cb1d0c2c09ac80
lizlaffitte <laffitte.digital@gmail.com>
2020-12-15 22:10:42 -0500

    Implement request spec to test user#create.

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
+    describe "POST users#create" do
+    it "creates a new user with valid attributes" do
+      user_params = {
+        user: {
+            username: 'TaterTot',
+            email: 'tatertot@aol.com',
+            password: 'potatoes'
+        }
+      }
+      post '/signup', :params => user_params.to_json, :headers => { "Content-Type": "application/json" }
+        json = JSON.parse(response.body)
+        expect(response).to have_http_status(201)
+    end
+
+  it "renders an error with invalid attributes" do
+    user_params = {
+      user: {
+          username: 'TaterTot',
+          email: 'tatertot@aol.com'
+      }
+    }
+    post '/signup', :params => user_params.to_json, :headers => { "Content-Type": "application/json" }
+      json = JSON.parse(response.body)
+      expect(json["error"]).to include("Password can't be blank")
+  end
+end
+
 
 end
+
