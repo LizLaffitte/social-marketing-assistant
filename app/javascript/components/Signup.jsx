0ed8e1529f6ec444b50338824d64b6285071f4f8
lizlaffitte <laffitte.digital@gmail.com>
2020-12-17 06:32:50 -0500

    Stub out signup form, using hooks.

---
+++ app/javascript/components/Signup.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+function Signup() {
+const [username, setUsername] = useState("")
+const [password, setPassword] = useState("")
+const [email, setEmail] = useState("")
+
+return(
+    <form>
+        <h1>Sign Up</h1>
+        <label>Username: </label><br />
+        <input type="text" value={username} onChange={e => setUsername(e.target.value)} name="username" /><br /><br />
+        <label>Email Address: </label><br />
+                <input type="email" value={email} onChange={e => setEmail(e.target.value)} name="email" /><br /><br />
+        <label>Password: </label> <br />
+        <input type="password" value={password} name="password" onChange={e => setPassword(e.target.value)} />
+        <br /><br />
+        <input type="submit" value="Sign up"  />
+    </form>
+)
+}
+
+export default Signup
