ab6d1eca284f8eca71cf7c27bbbef9b70a557967
lizlaffitte <laffitte.digital@gmail.com>
2020-12-17 06:32:31 -0500

    Add route for future signup component, flesh out login form.

--- app/javascript/components/Login.jsx
+++ app/javascript/components/Login.jsx
@@ -1,19 +1,19 @@
-import React, { Component, useState } from "react";
+import React, { useState } from "react";
 
 function Login() {
-const [username, setUsername] = useState("")
-const [password, setPassword] = useState("")
-return(
-    <form>
-        <h1>Login</h1>
-        <label>Username: </label><br />
-        <input type="text" value={username} onChange={this.handleOnChange} name="username" /><br /><br />
-        <label>Password: </label> <br />
-        <input type="password" value={password} name="password" onChange={this.handleOnChange} />
-        <br /><br />
-        <input type="submit" value="Log In"  />
-    </form>
-)
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    return(
+        <form>
+            <h1>Log in</h1>
+            <label>Username: </label><br />
+            <input type="text" value={username} onChange={e => setUsername(e.target.value)} name="username" /><br /><br />
+            <label>Password: </label> <br />
+            <input type="password" value={password} name="password" onChange={e => setPassword(e.target.value)} />
+            <br /><br />
+            <input type="submit" value="Log in"  />
+        </form>
+    )
 }
 
 export default Login
--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from '../components/Login'
 import Home from '../components/Home'
+import Signup from '../components/Signup'
 
 export default (
   <Router>
     <Switch>
         <Route path="/" exact component={Home}/>
-      <Route path="/login" exact component={Login} />
+      <Route path="/signup" exact component={Signup} />
+      <Route path='/login' exact component={Login} />
     </Switch>
   </Router>
 )
