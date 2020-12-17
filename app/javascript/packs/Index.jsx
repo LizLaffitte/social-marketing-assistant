000a4292decfe70369565ffbb176fcd499868c02
lizlaffitte <laffitte.digital@gmail.com>
2020-12-17 13:22:11 -0500

    Incorporate redux store.

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import ReactDOM, {render} from 'react-dom'
 import PropTypes from 'prop-types'
 import App from '../components/App'
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk'
+import userReducer from '../reducers/userReducer'
 
-
+const store = createStore(userReducer, compose(applyMiddleware(thunk)))
 document.addEventListener("DOMContentLoaded", () => {
   render(
-    <App />,
+    <Provider store={store}>
+    <App />
+    </Provider>,
     document.body.appendChild(document.createElement("div"))
   );
 });
