04fc1bdc2f50c57b76f1b1682565e1501b5005d8
lizlaffitte <laffitte.digital@gmail.com>
2020-12-17 06:11:50 -0500

    Set up basic login form component.

--- app/javascript/components/Login.jsx
+++ app/javascript/components/Login.jsx
@@ -1,10 +1,17 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 
 function Login() {
-
+const [username, setUsername] = useState("")
+const [password, setPassword] = useState("")
 return(
     <form>
-        <h2>Login</h2>
+        <h1>Login</h1>
+        <label>Username: </label><br />
+        <input type="text" value={username} onChange={this.handleOnChange} name="username" /><br /><br />
+        <label>Password: </label> <br />
+        <input type="password" value={password} name="password" onChange={this.handleOnChange} />
+        <br /><br />
+        <input type="submit" value="Log In"  />
     </form>
 )
 }
