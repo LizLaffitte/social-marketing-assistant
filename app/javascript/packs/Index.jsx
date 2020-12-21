1536e81186cf36ae98b3d7baf6bc313c7e9a895c
lizlaffitte <laffitte.digital@gmail.com>
2020-12-20 20:34:38 -0500

    Chnage Index.jsx useage of default reducer from user to rootReducer.

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -5,9 +5,9 @@ import App from '../components/App'
 import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk'
-import userReducer from '../reducers/userReducer'
+import rootReducer from '../reducers/rootReducer'
 
-const store = createStore(userReducer, compose(applyMiddleware(thunk)))
+const store = createStore(rootReducer, compose(applyMiddleware(thunk)))
 document.addEventListener("DOMContentLoaded", () => {
   render(
     <Provider store={store}>
