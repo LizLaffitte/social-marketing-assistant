fa08cd5144bf3bf7e5c25845a964001cd4a744bc
lizlaffitte <laffitte.digital@gmail.com>
2020-12-20 20:47:52 -0500

    Changed Routes to class component for better access to store and dispatch actions.

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, {Component} from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from '../components/Login'
 import Home from '../components/Home'
 import Signup from '../components/Signup'
+import {signup} from '../actions/userActions'
+import {connect} from 'react-redux'
 
-export default (
-  <Router>
-    <Switch>
-        <Route path="/" exact component={Home}/>
-      <Route path="/signup" exact component={Signup} />
-      <Route path='/login' exact component={Login} />
-    </Switch>
-  </Router>
-)
+class Routes extends Component {
+  render(){
+    return (
+      <>
+      <Router>
+        <Switch>
+            <Route path="/" exact component={Home}/>
+          <Route path="/signup" exact render={props => <Signup {...props} signup={this.props.signup} />} />
+          <Route path='/login' exact component={Login} />
+        </Switch>
+      </Router>
+      </>
+    )
+  }
+}
+
+export default connect(null, {signup})(Routes)
