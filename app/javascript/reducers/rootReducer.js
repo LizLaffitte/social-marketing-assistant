d8b5e2a6fa4935881f784e8b1235d6e8649137b7
lizlaffitte <laffitte.digital@gmail.com>
2020-12-26 14:01:56 -0500

    Set up current user reducer.

--- app/javascript/reducers/rootReducer.js
+++ app/javascript/reducers/rootReducer.js
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux'
 import userReducer from './userReducer'
+import currentUser from './currentUser'
 const rootReducer = combineReducers({
     users: userReducer,
-
+    currentUser
 })
 
 export default rootReducer
