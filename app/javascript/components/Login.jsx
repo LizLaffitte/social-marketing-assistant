c4b528d8cef07bf7df031b5ba711569e8b1bee35
lizlaffitte <laffitte.digital@gmail.com>
2020-12-26 14:01:09 -0500

    Set current user on login and redirect.

--- app/javascript/components/Login.jsx
+++ app/javascript/components/Login.jsx
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
 
-function Login() {
+function Login(props) {
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
+    const login = props.login
+    const handleOnSubmit = (e) => {
+        e.preventDefault()
+        login({username, password}, token)
+        props.history.push('/')
+    }
+    const token = document.querySelector('meta[name="csrf-token"]').content;
     return(
-        <form>
+        <form onSubmit={handleOnSubmit}>
             <h1>Log in</h1>
             <label>Username: </label><br />
             <input type="text" value={username} onChange={e => setUsername(e.target.value)} name="username" /><br /><br />
