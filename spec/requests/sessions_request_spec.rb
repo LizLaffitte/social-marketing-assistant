010ecd652ba8750c1d43b12f5b7c98045893fab2
lizlaffitte <laffitte.digital@gmail.com>
2020-12-26 14:03:13 -0500

    Comment out duplicated code.

--- spec/requests/sessions_request_spec.rb
+++ spec/requests/sessions_request_spec.rb
@@ -1,30 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe "Sessions", type: :request do
-    describe "POST sessions#create" do
-        it "creates a new user with valid attributes" do
-            user_params = {
-                user: {
-                    username: 'TaterTot',
-                    email: 'tatertot@aol.com',
-                    password: 'potatoes'
-                }
-            }
-            post '/signup', :params => user_params.to_json, :headers => { "Content-Type": "application/json" }
-                json = JSON.parse(response.body)["data"]["attributes"]
-                expect(json).to include("email")
-        end
+    # describe "POST sessions#create" do
+    #     it "logins in a user with valid attributes" do
+    #         user_params = {
+    #             user: {
+    #                 username: 'TaterTot',
+    #                 email: 'tatertot@aol.com',
+    #                 password: 'potatoes'
+    #             }
+    #         }
+    #         post '/login', :params => user_params.to_json, :headers => { "Content-Type": "application/json" }
+    #             json = JSON.parse(response.body)["data"]["attributes"]
+    #             expect(json).to include("email")
+    #     end
 
-        it "renders an error with invalid attributes" do
-            user_params = {
-            user: {
-                username: 'TaterTot',
-                email: 'tatertot@aol.com'
-            }
-            }
-            post '/signup', :params => user_params.to_json, :headers => { "Content-Type": "application/json" }
-            json = JSON.parse(response.body)
-            expect(json["error"]).to include("Password can't be blank")
-        end
-    end
+    #     it "renders an error with invalid attributes" do
+    #         user_params = {
+    #         user: {
+    #             username: 'TaterTot',
+    #             email: 'tatertot@aol.com'
+    #           }
+    #         }
+    #         post '/signup', :params => user_params.to_json, :headers => { "Content-Type": "application/json" }
+    #         json = JSON.parse(response.body)
+    #         expect(json["error"]).to include("Password can't be blank")
+    #     end
+    # end
 end
