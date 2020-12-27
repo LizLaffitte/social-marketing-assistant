95559549589c1bad8f67c4b33172507a684a028c
lizlaffitte <laffitte.digital@gmail.com>
2020-12-27 12:39:06 -0500

    ALlow for easy access to login and signup.

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 function Home(){
     return(
         <div className="main">
             <h1>Home</h1>
+            <ul>
+                <li><Link to="/login">Login</Link></li>
+                <li><Link to="/signup">Signup</Link></li>
+            </ul>
         </div>
     )
 }
