c2e26edf868335bfc3bd47db7c6eb02fc1f685fd
lizlaffitte <laffitte.digital@gmail.com>
2020-12-27 12:41:34 -0500

    Wrap app so we can use cookies.

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -6,13 +6,17 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk'
 import rootReducer from '../reducers/rootReducer'
+import { CookiesProvider } from 'react-cookie';
+
 
 const store = createStore(rootReducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__({ trace: true, traceLimit: 25 })))
 document.addEventListener("DOMContentLoaded", () => {
   render(
-    <Provider store={store}>
-    <App />
-    </Provider>,
+    <CookiesProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </CookiesProvider>,
     document.body.appendChild(document.createElement("div"))
   );
 });
