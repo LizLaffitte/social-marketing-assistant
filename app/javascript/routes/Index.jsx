bd9a651e4f7a40cef29e9b561967ca524db484bd
lizlaffitte <laffitte.digital@gmail.com>
2020-12-28 21:41:24 -0500

    Convert Routes to functional component so that we can use hooks.

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -1,38 +1,30 @@
-import React, {Component} from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import Login from '../components/Login'
 import Home from '../components/Home'
 import Signup from '../components/Signup'
-import {signup, login} from '../actions/userActions'
-import {connect} from 'react-redux'
-import { withCookies, Cookies } from 'react-cookie';
+import {useSelector} from 'react-redux'
 
 
 
-class Routes extends Component {
-  loggedIn = () =>{
-    return this.props.currentUser
-}
-
-  render(){
-    const {login, signup} = this.props
+function Routes() {
+  const loggedInUser = useSelector(state => state.currentUser)
     return (
       <>
       <Router>
         <Switch>
           <Route path="/" exact component={Home}/>
           <Route exact path='/login' >
-            {this.loggedIn() ? <Redirect to="/" /> : <Login login={login}  />}
+            {loggedInUser ? <Redirect to="/" /> : <Login />}
           </Route>
           <Route exact path='/signup' >
-            {this.loggedIn() ? <Redirect to="/" /> : <Signup signup={signup}  />}
+            {loggedInUser ? <Redirect to="/" /> : <Signup  />}
           </Route>
          
         </Switch>
       </Router>
       </>
     )
-  }
 }
-const mapStateToProps = ({currentUser, ownProps}) => ({currentUser, ownProps})
-export default withCookies(connect(mapStateToProps, {signup, login})(Routes))
+
+export default Routes
