36e46ef2e8f6228720550e603ba30615c742a5d1
lizlaffitte <laffitte.digital@gmail.com>
2021-01-02 19:51:34 -0500

    Set currentUser based on state and/or cookie.

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import Login from '../components/Login'
 import Home from '../components/Home'
 import Signup from '../components/Signup'
-import {useSelector} from 'react-redux'
-
+import {useSelector, useDispatch} from 'react-redux'
+import { useCookies } from 'react-cookie';
+import { clearCurrentUser, setCurrentUser } from "../actions/userActions";
 
 
 function Routes() {
   const loggedInUser = useSelector(state => state.currentUser)
+  const [cookies] = useCookies(['user']);
+  const dispatch = useDispatch()
+  useEffect(() => {
+    if(loggedInUser) {
+      dispatch(setCurrentUser(loggedInUser))
+    } else if (cookies["user"] != "undefined"){
+      dispatch(setCurrentUser(cookies["user"]))
+    }else {
+      dispatch(clearCurrentUser())
+    }
+  })
+ 
     return (
       <>
       <Router>
