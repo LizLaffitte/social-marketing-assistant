e6b07a269d416ced291e7d80edcb5336ce1b597d
lizlaffitte <laffitte.digital@gmail.com>
2021-01-03 19:12:24 -0500

    Matching User#show action to correct route.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   post '/signup', to: "users#create"
   post '/login', to: "sessions#create"
   delete '/logout', to: "sessions#destroy"
+
+  resources :users, only: [:show]
   
   root 'sessions#index'
   get '/*path' => 'sessions#index'
