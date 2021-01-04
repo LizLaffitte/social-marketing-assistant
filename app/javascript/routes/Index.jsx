d9ff25fe6a9cbf1158588ac97db3c6337734d7d2
lizlaffitte <laffitte.digital@gmail.com>
2021-01-03 19:15:23 -0500

    Correctly set cookie and/or login user based on cookie or user variable in the store.

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -5,20 +5,21 @@ import Home from '../components/Home'
 import Signup from '../components/Signup'
 import {useSelector, useDispatch} from 'react-redux'
 import { useCookies } from 'react-cookie';
-import { clearCurrentUser, setCurrentUser } from "../actions/userActions";
+import {getCurrentUser } from "../actions/userActions";
 
 
 function Routes() {
   const loggedInUser = useSelector(state => state.currentUser)
-  const [cookies] = useCookies(['user']);
+  const [cookies, setCookie] = useCookies(['user']);
   const dispatch = useDispatch()
+  const token = document.querySelector('meta[name="csrf-token"]').content;
   useEffect(() => {
-    if(loggedInUser) {
-      dispatch(setCurrentUser(loggedInUser))
-    } else if (cookies["user"] != "undefined"){
-      dispatch(setCurrentUser(cookies["user"]))
-    }else {
-      dispatch(clearCurrentUser())
+    if(loggedInUser && cookies["user"] == "undefined") {
+      console.log(loggedInUser.id.to_s)
+      setCookie('user', loggedInUser.id, {path: '/'})
+    } else if (!loggedInUser && cookies["user"] != "undefined"){
+      dispatch(getCurrentUser(cookies["user"]))
+
     }
   })
  
