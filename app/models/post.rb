878752a9145118f9250458b9fa6c714d584ff329
lizlaffitte <laffitte.digital@gmail.com>
2021-01-06 19:26:43 -0500

    Additional Post unit tests and validations to pass them.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
     validates :content, presence: true
     validates :platform, presence: true, inclusion: { in: %w(Twitter Facebook Instagram), message: "is not a valid platform" }
+    validates :status, presence: true, inclusion: { in: %w(draft approved pending scheduled), message: "is not a valid status" }
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  subject {Post.new(link:'http://google.com', content:'We love puppies! Check out all of these really cool pupperinos.', platform: "Twitter")}
+  subject {Post.new(link:'http://google.com', content:'We love puppies! Check out all of these really cool pupperinos.', platform: "Twitter", status: 'draft')}
 
   before {subject.save}
 
@@ -18,5 +18,14 @@ RSpec.describe Post, type: :model do
   it 'should have a valid platform' do
     subject.platform = "Pinterest"
     expect(subject).to_not be_valid
+  end
+
+  it 'should have a valid status' do
+    subject.status = "Single"
+    expect(subject).to_not be_valid
+  end
+
+  it 'should be valid with valid attributes' do
+    expect(subject).to be_valid
   end
 end
