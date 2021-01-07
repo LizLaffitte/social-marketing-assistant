3a7c64c51ae3d6c32fd4216c011e25c537e7714c
lizlaffitte <laffitte.digital@gmail.com>
2021-01-06 19:04:47 -0500

    Unit tests for Post.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {Post.new(link:'http://google.com', content:'We love puppies! Check out all of these really cool pupperinos.', platform: "Twitter")}
+
+  before {subject.save}
+
+  it 'should have content' do
+    subject.content = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should have a platform' do
+    subject.platform = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should have a valid platform' do
+    subject.platform = "Pinterest"
+    expect(subject).to_not be_valid
+  end
 end
