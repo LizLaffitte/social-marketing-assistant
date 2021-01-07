64d01ef7ba26c3d28f4c6ecf94a46540b80a8329
lizlaffitte <laffitte.digital@gmail.com>
2021-01-06 19:04:20 -0500

    Post validators for presence of concent and platform.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+    validates :content, presence: true
+    validates :platform, presence: true, inclusion: { in: %w(Twitter Facebook Instagram), message: "is not a valid platform" }
 end
