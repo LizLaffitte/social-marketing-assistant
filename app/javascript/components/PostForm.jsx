a8b3dabda6b9fcfd9991132601722085791cc518
lizlaffitte <laffitte.digital@gmail.com>
2021-01-07 19:19:23 -0500

    Implement onchange for other post form inputs, saving to state. Setting up for future dispatch.

--- app/javascript/components/PostForm.jsx
+++ app/javascript/components/PostForm.jsx
@@ -1,9 +1,11 @@
 import React, {useState} from 'react'
+import {useDispatch} from 'react-redux'
 
 function PostForm(){
     const [content, setContent] = useState("")
     const [platform, setPlatform] = useState("")
     const [status, setStatus] = useState("draft")
+    const dispatch = useDispatch()
     const handleOnSubmit = (e) => {
         e.preventDefault()
     }
@@ -14,8 +16,15 @@ function PostForm(){
                 <option value="facebook">Facebook</option>
                 <option value="twitter">Twitter</option>
                 <option value="instagram">Instagram</option>
-            </select><br /><br />
-            <textarea rows = "5" cols = "60" name ="content" placeholder="Write your awesome post here..." >
+            </select>
+            <select id="status" name="status"  value={status} onChange={e => setStatus(e.target.value)}>
+                <option value="facebook">Draft</option>
+                <option value="twitter">Pending</option>
+                <option value="instagram">Approved</option>
+                <option value="instagram">Scheduled</option>
+            </select>
+            <br /><br />
+            <textarea rows = "5" cols = "60" name ="content" value={content} onChange={e =>setContent(e.target.value)} placeholder="Write your awesome post here..." >
             </textarea><br /><br />
             <input type="submit" />
         </form>
