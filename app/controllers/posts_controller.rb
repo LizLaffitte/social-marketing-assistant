5561b562e7a0b5ea2fcfce280a2acf5ad94f7e27
lizlaffitte <laffitte.digital@gmail.com>
2021-01-17 14:49:35 -0500

    Removed orphan else clause in postsController.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,9 +2,9 @@ class PostsController < ApplicationController
     def create
         post = Post.new(post_params)
         render json: PostSerializer.new(post), status: :created
-        else 
-            render json: {error: post.errors.full_messages.to_sentence, status: :unprocessable_entity}
-        end
+        # else 
+        #     render json: {error: post.errors.full_messages.to_sentence, status: :unprocessable_entity}
+        # end
     end
 
     private
