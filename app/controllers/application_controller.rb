b77308b51ddb85567a15a0acd1b3b1168ad4078f
lizlaffitte <laffitte.digital@gmail.com>
2021-02-06 15:26:01 -0500

    Added helper methods for caluclating Twitter sig

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,21 @@ class ApplicationController < ActionController::Base
     def logged_in?
         !!session[:user_id]
     end
+
+    def sig_base(method, url, oauth_params)
+        sig = method.upcase + "&" + CGI.escape(url) + "&"
+        sorted_params = oauth_params.collect{|k,v| [ URI.escape(k), URI.escape(v)] }.sort
+        sig + CGI.escape(sorted_params.collect!{|arr| arr.join("=")}.join("&"))
+    end
+    
+    def sig_key
+        CGI.escape(ENV['SECRET']) + "&" 
+    end
+
+    def sig_generator(method, url, oauth_params)
+        base = sig_base(method, url, oauth_params)
+        key = sig_key
+        Base64.strict_encode64(OpenSSL::HMAC.hexdigest("SHA256", key, base))
+    end
+
 end
