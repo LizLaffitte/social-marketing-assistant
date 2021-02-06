ff14bb8e8aab2b0d169bf7b3220a23e961c54c7e
lizlaffitte <laffitte.digital@gmail.com>
2021-02-06 14:57:42 -0500

    Disregard onmiauth adds.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :twitter, ENV['KEY'], ENV['SECRET']
-end
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#     provider :twitter, ENV['KEY'], ENV['SECRET']
+# end
