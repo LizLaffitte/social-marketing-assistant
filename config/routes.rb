09f7919bbbdc224d981e807c386ef9fb34d5226f
lizlaffitte <laffitte.digital@gmail.com>
2021-02-06 14:56:55 -0500

    Fix routes to match oauth flow.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   post '/signup', to: "users#create"
   post '/login', to: "sessions#create"
   delete '/logout', to: "sessions#destroy"
-  get '/auth/twitter/callback', to: 'sessions#create'
+  get '/auth/twitter', to: "social#create"
+  post '/auth/:provider/callback', to: 'sessions#create'
   resources :users, only: [:show]
   
   resources :clients do
