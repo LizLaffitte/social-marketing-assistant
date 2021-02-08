93a96a52bf39cf4e050a47bf07648b0b4f968df0
lizlaffitte <laffitte.digital@gmail.com>
2021-02-07 21:26:07 -0500

    Succssfully implement Twitter 3-leg oauth.

--- app/controllers/social_controller.rb
+++ app/controllers/social_controller.rb
@@ -1,24 +1,39 @@
 require 'pry'
+require 'oauth'
 class SocialController < ApplicationController
     def create
-        oauth_params = {
-            'oauth_callback' =>  CGI.escape('http://127.0.0.1:3000/auth/twitter/callback'),
-            'oauth_consumer_key' => ENV['KEY'],
-            'oauth_nonce' => SecureRandom.hex, 
-            'oauth_signature_method' => 'HMAC-SHA1',
-            'oauth_version' => '1.0',
-            'oauth_timestamp' => Time.now.to_i.to_s
-        }
-        baseUrl = 'https://api.twitter.com/oauth/request_token'
-        signature = sig_generator("POST", baseUrl, oauth_params)
-        oauth_params['Content-Type'] ='application/json'
-        oauth_params['oauth_signature'] = signature
-        req_token = HTTParty.post(baseUrl,
-      :headers => oauth_params
-      )
-      render json: req_token.body
+        ckey = ENV['KEY']
+        csecret = ENV['SECRET']
+        consumer = OAuth::Consumer.new(ckey,csecret,
+            :site => 'https://api.twitter.com',
+            :authorize_path => '/oauth/authenticate',
+            :debug_output => true)
+        callback_url = "http://127.0.0.1:3000/auth/twitter/callback"
+        request_token = consumer.get_request_token(:oauth_callback => callback_url)
+        token = request_token.token
+        token_secret = request_token.secret
+        confirmed = request_token.params["oauth_callback_confirmed"]
+        if confirmed === "true"
+            redirect_to "https://api.twitter.com/oauth/authorize?oauth_token=#{token}"
+        else
+            redirect_to "/"
+        end
     end
 
+    def index
+        oauth_token = params["oauth_token"]
+        oauth_verifier = params["oauth_verifier"]
+        baseUrl = 'https://api.twitter.com/oauth/access_token'
+        response = HTTParty.post(baseUrl + "?oauth_token=#{oauth_token}&oauth_verifier=#{oauth_verifier}" )
+        @access_token = response.split("&")[0]
+        @access_token.slice!("oauth_token=")
+        @access_secret = response.split("&")[1]
+        @access_secret.slice!("oauth_token_secret=")
+        redirect_to "/"
+    end
+
+    def post_tweet
+    end
 
 
     private
