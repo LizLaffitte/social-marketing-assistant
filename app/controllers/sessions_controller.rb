359f947cb1d9f532b1cc9c6d3ce0a8d65975f92b
lizlaffitte <laffitte.digital@gmail.com>
2021-02-08 21:17:28 -0500

    Add functionality to login users with Twitter.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,33 @@
 require 'pry'
+
 class SessionsController < ApplicationController
+    include SessionsHelper
     def create
-        # binding.pry
-        if params[:username]
+        if params[:oauth_token] && params[:oauth_verifier]
+            oauth_token = params["oauth_token"]
+            oauth_verifier = params["oauth_verifier"]
+            baseUrl = 'https://api.twitter.com/oauth/access_token'
+            response = HTTParty.post(baseUrl + "?oauth_token=#{oauth_token}&oauth_verifier=#{oauth_verifier}" )
+            @access_token = response.split("&")[0].split("=")[1]
+            @access_secret = response.split("&")[1].split("=")[1]
+            @user_id = response.split("&")[2].split("=")[1]
+            @user_sn = response.split("&")[3].split("=")[1]
+            user = User.find_by(username: @user_sn)
+            if user
+                session[:user_id] = user.id
+                render json: UserSerializer.new(user)
+            else
+                new_user_info = get_user(@user_sn)
+                # new_user = User.new(username: @user_sn, password: SecureRandom.hex, uid: @user_id, image: auth['info']['image'] )
+                #   if @user.save
+                #     log_in(@user)
+                #   else
+                #     render :new
+                #   end
+                render json: new_user_info
+
+            end
+        elsif params[:username]
             user = User.find_by(username: params[:username])
             if user && user.authenticate(params[:password])
                 session[:user_id] = user.id
@@ -13,7 +38,7 @@ class SessionsController < ApplicationController
                 render json: {errors: "Invalid credentials"}
             end
         else
-
+            redirect_to "/login"
         end
     end
 
@@ -22,6 +47,5 @@ class SessionsController < ApplicationController
         render json: {notice: "Successfully logged out"}
     end
 
-    def index
-    end
+
 end
