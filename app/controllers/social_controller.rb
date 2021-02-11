bc9ea5aadc570c0f5e39ec364b8c9b2c97e3704a
lizlaffitte <laffitte.digital@gmail.com>
2021-02-10 20:59:11 -0500

    Move req_access and get_user into the social controller to better encapsulate Twitter API calls.

--- app/controllers/social_controller.rb
+++ app/controllers/social_controller.rb
@@ -16,10 +16,31 @@ class SocialController < ApplicationController
         if confirmed === "true"
             redirect_to "https://api.twitter.com/oauth/authorize?oauth_token=#{token}"
         else
-            redirect_to "/"
+            redirect_to "/login"
         end
     end
 
+    def req_access
+        oauth_token = params["oauth_token"]
+        oauth_verifier = params["oauth_verifier"]
+        baseUrl = 'https://api.twitter.com/oauth/access_token'
+        response = HTTParty.post(baseUrl + "?oauth_token=#{oauth_token}&oauth_verifier=#{oauth_verifier}" )
+        session[:access_token] = response.split("&")[0].split("=")[1]
+        session[:access_secret] = response.split("&")[1].split("=")[1]
+        session[:user_id] = response.split("&")[2].split("=")[1]
+        session[:screenname] = response.split("&")[3].split("=")[1]
+        response = get_user(screenname)
+        
+        render json: response
+        # redirect_to :controller => 'sessions', :action => 'create'
+    end
+
+    def get_user(username)
+        baseUrl = 'https://api.twitter.com/1.1/users/show.json'
+        header = {'authorization' => 'Bearer ' + ENV['BEARER']}
+        response = HTTParty.get(baseUrl + "?screen_name=#{username}", :headers => header)
+        return response
+    end
 
     private
 
