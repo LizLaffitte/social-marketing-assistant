f453d0ba9911ec2f021e6dea8ae11cb7eb5beda4
lizlaffitte <laffitte.digital@gmail.com>
2021-02-10 20:28:19 -0500

    Set stage for requests to be moved into helper methods.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,28 @@ class ApplicationController < ActionController::Base
         session[:user_id] = user.id
         redirect_to "/"    
     end
+
+    def req_access(token, verifier)
+        oauth_token = params["oauth_token"]
+        oauth_verifier = params["oauth_verifier"]
+        baseUrl = 'https://api.twitter.com/oauth/access_token'
+        response = HTTParty.post(baseUrl + "?oauth_token=#{oauth_token}&oauth_verifier=#{oauth_verifier}" )
+
+        session[:access_token] = response.split("&")[0].split("=")[1]
+        session[:access_secret] = response.split("&")[1].split("=")[1]
+        session[:user_id] = response.split("&")[2].split("=")[1]
+        session[:screename] = response.split("&")[3].split("=")[1]
+    end
+
+    def access_token
+        session[:access_token]
+    end
+
+    def access_secret
+        session[:access_secret]
+    end
+
+    def screename
+        session[:screenname]
+    end
 end
